ad26ae8b5f2d6f842767f845591d7197f7c24719
lettieviljoen <lettieviljoen@hotmail.com>
2025-07-19 13:06:20 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Roadrunner Self'
+project = 'Rordrunhelpdocs'
 copyright = '2025'
 author = 'Gert Vlok'
 
